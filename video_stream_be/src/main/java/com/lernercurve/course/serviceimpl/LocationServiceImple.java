b0b0d35880eb22a7f78cd76fedd40d33e79494e4
PRIYABRAT PANDA <priyabrat.panda@eidiko-india.com>
2024-12-20 22:13:30 +0530

    Update LocationServiceImple.java

--- video_stream_be/src/main/java/com/lernercurve/course/serviceimpl/LocationServiceImple.java
+++ video_stream_be/src/main/java/com/lernercurve/course/serviceimpl/LocationServiceImple.java
@@ -2,7 +2,9 @@ package com.lernercurve.course.serviceimpl;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +14,10 @@ import com.lernercurve.course.entity.VideoUploadLocation;
 import com.lernercurve.course.repository.VideoUploadLocationRepository;
 import com.lernercurve.course.service.LocationService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class LocationServiceImple implements LocationService {
 
     @Autowired
@@ -31,6 +36,37 @@ public class LocationServiceImple implements LocationService {
         var savedData = this.videoUploadLocationRepository.save(videoUploadLocation);
         responseMap.put("response", savedData);
         return responseMap;
+    }
+    
+    public static void main(String[] args) {
+        // Example list of CompletableFuture objects
+        List<CompletableFuture<Void>> futureList = List.of(
+            CompletableFuture.runAsync(() -> {
+            	log.info("current thread:- {}",Thread.currentThread());
+            	try {
+					Thread.sleep(10000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+            	log.info("Task 1");
+//            	return "Task 1";
+            }),
+            CompletableFuture.runAsync(() -> {
+            	log.info("current thread:- {}",Thread.currentThread());
+            	log.info("Task 2");
+//            	return "Task 2";
+            }),
+            CompletableFuture.runAsync(() -> {
+            	log.info("current thread:- {}",Thread.currentThread());
+            	log.info("Task 3");
+//            	return "Task 3";
+            })
+        );
+
+        // Convert List<CompletableFuture<String>> to CompletableFuture<String>[]
+       CompletableFuture.allOf(futureList.toArray(new CompletableFuture[futureList.size()])).join();
+      
     }
 
 }
